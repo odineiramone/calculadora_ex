d2922cc220ae3e466f7298c85ae5ad2b18b6c3e0
Odinei Ribeiro <odinei.ribeiro92@gmail.com>
2024-08-24 02:14:54 -0300

    Refatoração e teste

--- lib/calculadora_ex.ex
+++ lib/calculadora_ex.ex
@@ -1,45 +1,29 @@
-defmodule CalculadorEx do
-  def calcular(:+, n1, n2), do: adicao(n1, n2)
-  def calcular(:/, n1, n2), do: divisao(n1, n2)
-  def calcular(:*, n1, n2), do: multiplicacao(n1, n2)
+defmodule CalculadoraEx do
+  def calcular(op, n1, n2) when op in [:+, :-, :*, :/] do
+    with {:ok, n1} <- n1 |> valida_numero(),
+         {:ok, n2} <- n2 |> valida_numero(),
+         do: efetua_operacao(op, n1, n2)
+  end
 
   def calcular(_, _, _), do: {:error, :operacao_indisponivel}
 
-  def adicao(primeiro_numero, segundo_numero) do
-    with {:ok, n1} <- primeiro_numero |> valida_numero(),
-         {:ok, n2} <- segundo_numero |> valida_numero(),
-         do: efetua_adicao(n1, n2)
+  defp efetua_operacao(:+, n1, n2), do: adicao!(n1, n2)
+  defp efetua_operacao(:-, n1, n2), do: subtracao!(n1, n2)
+  defp efetua_operacao(:*, n1, n2), do: multiplicacao!(n1, n2)
+
+  defp efetua_operacao(:/, n1, n2) do
+    with {:ok, n2} <- n2 |> valida_divisor(),
+         do: divisao!(n1, n2)
   end
 
-  def divisao(primeiro_numero, segundo_numero) do
-    with {:ok, n1} <- primeiro_numero |> valida_numero(),
-         {:ok, _} <- segundo_numero |> valida_numero(),
-         {:ok, n2} <- segundo_numero |> valida_divisor(),
-         do: efetua_divisao(n1, n2)
-  end
-
-  def subtracao(primeiro_numero, segundo_numero) do
-    primeiro_numero - segundo_numero
-  end
-
-  def multiplicacao(primeiro_numero, segundo_numero) do
-    with {:ok, n1} <- primeiro_numero |> valida_numero(),
-         {:ok, n2} <- segundo_numero |> valida_numero(),
-         do: efetua_multiplicacao(n1, n2)
-  end
-
-  def efetua_multiplicacao(n1, n2), do: n1 * n2
-
-  def efetua_divisao(n1, n2), do: n1 / n2
-
-  defp efetua_adicao(n1, n2), do: n1 + n2
+  def adicao!(n1, n2), do: n1 + n2
+  def subtracao!(n1, n2), do: n1 - n2
+  def multiplicacao!(n1, n2), do: n1 * n2
+  def divisao!(n1, n2), do: n1 / n2
 
   defp valida_divisor(divisor) when divisor != 0, do: {:ok, divisor}
   defp valida_divisor(_), do: {:error, :divisor_igual_zero}
 
-  defp valida_numero(numero) when is_integer(numero) do
-    {:ok, numero}
-  end
-
+  defp valida_numero(numero) when is_integer(numero), do: {:ok, numero}
   defp valida_numero(_), do: {:error, :numero_invalido}
 end
--- test/calculadora_ex_test.exs
+++ test/calculadora_ex_test.exs
@@ -1,8 +1,47 @@
 defmodule CalculadoraExTest do
   use ExUnit.Case
-  doctest CalculadoraEx
 
-  test "greets the world" do
-    assert CalculadoraEx.hello() == :world
+  test "adicao!/2 soma dois valores" do
+    assert CalculadoraEx.adicao!(1, 2) == 3
+  end
+
+  test "subtracao!/2 subtrai dois valores" do
+    assert CalculadoraEx.subtracao!(3, 2) == 1
+  end
+
+  test "multiplicacao!/2 multiplica dois valores" do
+    assert CalculadoraEx.multiplicacao!(4, 2) == 8
+  end
+
+  test "divisao!/2 divide dois valores" do
+    assert CalculadoraEx.divisao!(6, 2) == 3
+  end
+
+  test "calcular/3 realiza somas" do
+    assert CalculadoraEx.calcular(:+, 1, 2) == 3
+  end
+
+  test "calcular/3 realiza subtrações" do
+    assert CalculadoraEx.calcular(:-, 5, 3) == 2
+  end
+
+  test "calcular/3 realiza multiplicações" do
+    assert CalculadoraEx.calcular(:*, 4, 3) == 12
+  end
+
+  test "calcular/3 realiza divisões" do
+    assert CalculadoraEx.calcular(:/, 9, 3) == 3
+  end
+
+  test "calcular/3 retorna tupla de erro com operações inválidas" do
+    assert CalculadoraEx.calcular(:=, 9, 3) == {:error, :operacao_indisponivel}
+  end
+
+  test "calcular/3 retorna tupla de erro com valores não-inteiros" do
+    assert CalculadoraEx.calcular(:+, "teste", 2.0) == {:error, :numero_invalido}
+  end
+
+  test "calcular/3 retorna tupla de erro com divisão por zero" do
+    assert CalculadoraEx.calcular(:/, 1, 0) == {:error, :divisor_igual_zero}
   end
 end
